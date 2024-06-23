40d015018a981d4574c51e800363b9a18940c0d1
GautamMishra20 <157229975+GautamMishra20@users.noreply.github.com>
2024-06-23 13:57:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3 ) //we are using nodejs, not browser
+
+let name = "Gautam"
+let age = 5
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => use for uniqueness
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
