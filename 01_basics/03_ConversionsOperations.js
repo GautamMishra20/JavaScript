728d75225acec518763138752a86a913f9bf50be
GautamMishra20 <157229975+GautamMishra20@users.noreply.github.com>
2024-06-23 14:44:59 +0000

    Conversions

---
+++ 01_basics/03_ConversionsOperations.js
@@ -0,0 +1,22 @@
+//let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "someValueInIt" => true
