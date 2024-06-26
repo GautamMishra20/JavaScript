d48d43011a62390a356c4bc4b87f9d9c6a705447
GautamMishra20 <157229975+GautamMishra20@users.noreply.github.com>
2024-06-26 18:55:30 +0000

    conversions

--- 01_basics/03_ConversionsOperations.js
+++ 01_basics/03_ConversionsOperations.js
@@ -11,12 +11,53 @@
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanisLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanisLoggedIn)
+// console.log(booleanisLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false
 // "someValueInIt" => true
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************************************** Operations ***************************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1"+3);
+// console.log(1+"3")
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+let str1 = "Hello "
+let str2 = "Gautam"
+let str3 = str1 + str2
+//console.log(str3)
+
+let gameCounter = 100
+//gameCounter++; --->> 101
+++gameCounter; //--->> 101
+console.log(gameCounter);
+
+let x = 3
+const y = x++
+console.log(x, y)
+
+let a = 3
+const b = ++a
+console.log(a, b);
