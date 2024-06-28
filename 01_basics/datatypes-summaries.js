622269816b1e16c834849b9b3eed971743a38404
GautamMishra20 <157229975+GautamMishra20@users.noreply.github.com>
2024-06-28 07:24:12 +0000

    Stack and Heap memory in file

--- 01_basics/datatypes-summaries.js
+++ 01_basics/datatypes-summaries.js
@@ -30,3 +30,32 @@ const myFunction = function(){
 }
 
 console.log(typeof bigNumber);
+
+
+//************************************************************************************************************************************
+
+// Stack(Primittive) , Heap(Non-Primittive)
+
+// stack gives a copy of the value of one variable to the value of the another variable.
+let mywork = "Engineering"
+
+let anotherWork = mywork
+anotherWork = "ui&ux_designer"
+console.log(mywork);
+console.log(anotherWork);
+
+// heap gives a reference of the one variable to the other variable.
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+userTwo.email = "gautam@gamil.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
