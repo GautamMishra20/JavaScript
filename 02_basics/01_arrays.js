46cf825eda2c30b38332609325ad9b0d76879e8d
GautamMishra20 <157229975+GautamMishra20@users.noreply.github.com>
2024-07-03 03:22:43 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+// Arrays:-
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["Ironman","Thor","Hulk"]
+
+const myArr2 = new Array(1,3,5,7,8)
+console.log(myArr[0]);
+console.log(myArr2[3]);
+
+// Array Methods:-
+myArr.push(6)
